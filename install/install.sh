565c288531e3e543788305fc19590677cab1b6a2
Mark Rawling <msr2406@gmail.com>
2020-11-28 16:56:09 +0000

    Added Version

--- install/install.sh
+++ install/install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
+repo="msrgit/workflow"
 echo -e "Installation started"
-
+readonly PISCREEN_VERSION=$(curl -s "https://api.github.com/repos/$repo/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")' )
+echo "Version: $PISCREEN_VERSION"
 sleep 3
 
 echo -e "Installation finished"
